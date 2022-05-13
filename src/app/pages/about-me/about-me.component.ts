f4a7731b27ac0327533c99322b3e948a34f074d1
CodeCrusher <rdramlal@outlook.com>
2022-05-13 20:09:26 +0200

    Adjusted settings of prettier ot be more of my liking.

--- src/app/pages/about-me/about-me.component.ts
+++ src/app/pages/about-me/about-me.component.ts
@@ -16,10 +16,7 @@ export class AboutMeComponent implements OnInit {
   databases: Skill[] = [];
   tools: Skill[] = [];
 
-  constructor(
-    private httpClient: HttpClient,
-    private skillsService: SkillsService
-  ) {
+  constructor(private httpClient: HttpClient, private skillsService: SkillsService) {
     this.getLanguages();
     this.getFrameworksLibraries();
     this.getPlatforms();
@@ -30,9 +27,7 @@ export class AboutMeComponent implements OnInit {
   ngOnInit(): void {}
 
   private getLanguages() {
-    this.skillsService
-      .getSkillData(SkillTypes.Languages)
-      .forEach(skills => this.languages.push(...skills));
+    this.skillsService.getSkillData(SkillTypes.Languages).forEach(skills => this.languages.push(...skills));
   }
 
   private getFrameworksLibraries() {
@@ -42,20 +37,14 @@ export class AboutMeComponent implements OnInit {
   }
 
   private getPlatforms() {
-    this.skillsService
-      .getSkillData(SkillTypes.Platforms)
-      .forEach(skills => this.platforms.push(...skills));
+    this.skillsService.getSkillData(SkillTypes.Platforms).forEach(skills => this.platforms.push(...skills));
   }
 
   private getDatabases() {
-    this.skillsService
-      .getSkillData(SkillTypes.Databases)
-      .forEach(skills => this.databases.push(...skills));
+    this.skillsService.getSkillData(SkillTypes.Databases).forEach(skills => this.databases.push(...skills));
   }
 
   private getTools() {
-    this.skillsService
-      .getSkillData(SkillTypes.Tools)
-      .forEach(skills => this.tools.push(...skills));
+    this.skillsService.getSkillData(SkillTypes.Tools).forEach(skills => this.tools.push(...skills));
   }
 }
