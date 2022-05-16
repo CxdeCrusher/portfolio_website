4471723995dad54401232dbe4bb11750c9a02fad
CodeCrusher <rdramlal@outlook.com>
2022-05-16 18:50:00 +0200

    Add fontawesome

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { library } from '@fortawesome/fontawesome-svg-core';
 
 @Component({
   selector: 'app-footer',
@@ -7,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
   currentYear: number;
+
 
   constructor() {
     this.currentYear = new Date().getFullYear();
